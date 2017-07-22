4029c9a363dd1b1e0de398e389f7dae8353871ee
Nishant Gadihoke <ngadihoke8@gmail.com>
2017-07-22 14:54:40 +0530

    Shifted sessions to production

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
@@ -13,10 +15,13 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 
 //Setting up sessions+cookies
-app.use(require('express-session')({
+app.use(session({
     secret: 'JagdishKumar',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
